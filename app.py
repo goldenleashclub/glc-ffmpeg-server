a46314330ff0bf7b9cf74eada4aa8ec59ce8cbd2
goldenleashclub <tim@goldenleashclub.com>
2026-05-16 17:10:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -48,6 +48,3 @@ def extract():
         "status": "done",
         "frames_extracted": frame_count
     })
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
